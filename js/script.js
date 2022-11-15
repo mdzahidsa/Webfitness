553add048852ff9cde3f059f869d9b1b45dbad83
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2022-11-15 09:11:48 +0000

    commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+// Product Script
+let i;
+let cartbtn=document.querySelectorAll("#membbtn");
+for (i = 0; i < cartbtn.length; i++) {
+  cartbtn[i].addEventListener("click",function(){
+  alert(this.innerText + " Added to Cart");
+});
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); 
+}
+
